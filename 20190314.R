3010de3ffefcf46a1fd25fe8b1a3602567e51e6d
jason19970210 <jason19970210@gmail.com>
2019-03-14 10:03:24 +0800

    New

--- 20190314.R
+++ 20190314.R
@@ -2,7 +2,14 @@
 # will run as 1,2,3,4,5,6 + 3,4,5,3,4,5
 # result will be 4  6  8  7  9 11
 iris$Time <- 1
-View(iris) #Capital 'V'iew
+View(iris)
+# Capital 'V'iew
+# Check list `iris`
 
 b <- seq(2,20,2)
+
+
 b[c(5,6,7)]
+b[5:7] # equals to b[c(5:7)
+# get 5th to 7th element
+
