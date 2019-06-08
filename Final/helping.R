ac4552e08a52d03fa092a9f8a61c7e18992d94b9
jason19970210 <jason19970210@gmail.com>
2019-06-08 18:49:12 +0800

    New

---
+++ Final/helping.R
@@ -0,0 +1,83 @@
+library(xml2)
+library(dplyr)
+
+
+url <- "https://raw.githubusercontent.com/jason19970210/BigDataAnalyticalMethods/master/Final/earthquake/CWB-EQ-Catalog-1990.xml"
+
+earthq_xml <- read_xml(url)
+earthq <- xml_find_all(earthq_xml, ".//earthquakeinfo")
+
+epicenterLon <- earthq %>% xml_find_first(".//epicenterLon ") %>% xml_text()
+#epicenterLon_unit <- earthq %>% xml_find_first(".//epicenterLon ") %>% xml_attr("unit")
+epicenterLat <- earthq %>% xml_find_first(".//epicenterLat ") %>% xml_text()
+#epicenterLat_unit <- earthq %>% xml_find_first(".//epicenterLat ") %>% xml_attr("unit")
+depth <- earthq %>% xml_find_first("./depth ") %>% xml_text()
+#depth_unit <- earthq %>% xml_find_first("./depth ") %>% xml_attr("unit")
+#magnitudeType <- earthq %>% xml_find_first(".//magnitudeType ") %>% xml_text()
+magnitudeValue <- earthq %>% xml_find_first(".//magnitudeValue ") %>% xml_text()
+minimumDistance <- earthq %>% xml_find_first("./minimumDistance ") %>% xml_text()
+#minimumDistance_unit <- earthq %>% xml_find_first("./minimumDistance ") %>% xml_attr("unit")
+gap <- earthq %>% xml_find_first("./gap ") %>% xml_text()
+#gap_unit <- earthq %>% xml_find_first("./gap ") %>% xml_attr("unit")
+rms <- earthq %>% xml_find_first("./rms ") %>% xml_text()
+#rms_unit <- earthq %>% xml_find_first("./rms ") %>% xml_attr("unit")
+erh <- earthq %>% xml_find_first("./erh ") %>% xml_text()
+#erh_unit <- earthq %>% xml_find_first("./erh ") %>% xml_attr("unit")
+erz <- earthq %>% xml_find_first("./erz ") %>% xml_text()
+#erz_unit <- earthq %>% xml_find_first("./erz ") %>% xml_attr("unit")
+
+earthq1990_df <- data.frame(originTime = xml_text(xml_find_first(earthq, ".//originTime")),
+                            epicenterLon = epicenterLon,
+                            #epicenterLon_unit = epicenterLon_unit,
+                            epicenterLat = epicenterLat,
+                            #epicenterLat_unit = epicenterLat_unit,
+                            depth = depth,
+                            #depth_unit = depth_unit,
+                            #magnitudeType = magnitudeType,
+                            magnitudeValue = magnitudeValue,
+                            phaseNumber = xml_text(xml_find_first(earthq, "./phaseNumber")),
+                            minimumDistance = minimumDistance,
+                            #minimumDistance_unit = minimumDistance_unit,
+                            gap = gap,
+                            #gap_unit = gap_unit,
+                            rms = rms,
+                            #rms_unit = rms_unit,
+                            erh = erh,
+                            #erh_unit = erh_unit,
+                            erz = erz)
+                            #erz_unit = erz_unit
+
+
+
+#https://stackoverflow.com/questions/25346430/dealing-with-empty-xml-nodes-in-r
+if (1 < 0){
+  xmlGetNodeAttr <- function(n, xp, attr, default=NA) {
+    ns<-getNodeSet(n, xp)
+    if(length(ns)<1) {
+      return(default)
+    } else {
+      sapply(ns, xmlGetAttr, attr, default)
+    }
+  }
+
+a <- fname=xmlGetNodeAttr(eq1990, "./earthquakeinfo","originTime",NA)
+data.frame(
+  lname=xmlGetNodeAttr(x, "./Info","lastNm",NA),
+  orgname=xmlGetNodeAttr(x, "./CrntEmps/CrntEmp[1]","orgNm",NA),
+  zip=xmlGetNodeAttr(x, "./CrntEmps/CrntEmp[1]/BrnchOfLocs/BrnchOfLoc[1]","city",NA)
+)
+}
+
+
+library(chron)
+dtimes = c("2002-06-09 12:45:40","2003-01-29 09:30:40","2002-09-04 16:45:40","2002-11-13 20:00:40","2002-07-07 17:30:40")
+dtparts = t(as.data.frame(strsplit(dtimes,' ')))
+row.names(dtparts) = NULL
+thetimes = chron(dates=dtparts[,1],times=dtparts[,2],format=c('y-m-d','h:m:s'))
+thetimes
+
+
+
+
+a <- 3+NA
+a
--- Final/main.R
+++ Final/main.R
@@ -4,65 +4,72 @@
 # Library Import
 #library(RCurl)
 #library(rvest)
+#library(tibble) #http://blog.fens.me/r-tibble/
 library(xml2)
-#library(dplyr)
+library(dplyr)
 library(ggplot2)
 library(tidyverse)
+library(purrr)
+
 
 # Web Crawling (Typhoon) >> NOT WORKING
 
 # Data Import (Typhoon, Earthquake)
-typhoon <- read.csv("https://raw.githubusercontent.com/jason19970210/BigDataAnalyticalMethods/master/Final/typhoon_web_table.csv",stringsAsFactors = F)
+typhoon_all <- read.csv("https://raw.githubusercontent.com/jason19970210/BigDataAnalyticalMethods/master/Final/typhoon_web_table.csv",stringsAsFactors = F)
 xml_url_base <- paste("https://raw.githubusercontent.com/jason19970210/BigDataAnalyticalMethods/master/Final/earthquake/CWB-EQ-Catalog-%d","xml",sep = ".")
 #eq1990 <- read_xml("https://raw.githubusercontent.com/jason19970210/BigDataAnalyticalMethods/master/Final/earthquake/CWB-EQ-Catalog-1990.xml", Encoding = "utf-8")
+#a <- as.list(xml_text(xml_find_all(eq1990,".//phaseNumber")))
+#b <- as.list(xml_text(xml_find_all(eq1990,".//originTime")))
+#eq1990 %>% xml_child() %>% map(xml_find_all, ".//phaseNumber") %>% map_if(is_empty, ~{.eq1990 <- NA})
+
+
 map_df(1990:2018, function(i){
   xml_url <- read_xml(sprintf(xml_url_base,i))
+  eqinfo <- xml_find_all(xml_url, ".//earthquakeinfo")
   
-  data.frame(originTime = xml_text(xml_find_all(xml_url, ".//originTime")),
-             epicenterLon = xml_text(xml_find_all(xml_url, ".//epicenterLon")),
-             epicenterLat = xml_text(xml_find_all(xml_url, ".//epicenterLat")),
-             depth = xml_text(xml_find_all(xml_url, ".//depth")),
-             magnitudeValue = xml_text(xml_find_all(xml_url, ".//magnitudeValue")),
-             stationNumber = xml_text(xml_find_all(xml_url, ".//stationNumber")),
-             phaseNumber = as.list(xml_text(xml_find_all(xml_url, ".//phaseNumber"))),
-             
-             stringsAsFactors = FALSE
-  )
+  #tibble::tibble
+  data.frame(originTime = xml_text(xml_find_first(eqinfo, ".//originTime")),
+             epicenterLon = xml_text(xml_find_first(eqinfo, ".//epicenterLon")),
+             epicenterLat = xml_text(xml_find_first(eqinfo, ".//epicenterLat")),
+             depth = xml_text(xml_find_first(eqinfo, ".//depth")),
+             magnitudeValue = xml_text(xml_find_first(eqinfo, ".//magnitudeValue")),
+             stationNumber = xml_text(xml_find_first(eqinfo, ".//stationNumber")),
+             phaseNumber = xml_text(xml_find_first(eqinfo, "./phaseNumber")),
+             minimumDistance = xml_text(xml_find_first(eqinfo, "./minimumDistance")),
+             gap = xml_text(xml_find_first(eqinfo, "./gap")),
+             rms = xml_text(xml_find_first(eqinfo, "./rms")),
+             erh = xml_text(xml_find_first(eqinfo, "./erh")),
+             erz = xml_text(xml_find_first(eqinfo, "./erz")),
+             quality = xml_text(xml_find_first(eqinfo, "./quality")),
+             reviewStatus = xml_text(xml_find_first(eqinfo, "./reviewStatus")),
+            
+             stringsAsFactors = FALSE   #doesnt have this parameter in tibble
+  ) #end of data.frame
 }) -> eq_df
 
-map_df(1990:2018, function(i){
-  
-  xml_url <- read_xml(sprintf(xml_url_base,i))
-  
-  cbind(originTime = xml_text(xml_find_all(xml_url, ".//originTime")),
-        phaseNumber = xml_text(xml_find_all(xml_url, ".//phaseNumber")),
-        minimumDistance = xml_text(xml_find_all(xml_url, ".//minimumDistance")),
-        gap = xml_text(xml_find_all(xml_url, ".//gap")),
-        rms = xml_text(xml_find_all(xml_url, ".//rms")),
-        erh = xml_text(xml_find_all(xml_url, ".//erh")),
-        erz = xml_text(xml_find_all(xml_url, ".//erz")),
-        quality = xml_text(xml_find_all(xml_url, ".//quality")),
-        reviewStatus = xml_text(xml_find_all(xml_url, ".//reviewStatus")),
-        
-        stringsAsFactors = F)
-}) -> eq_df_tmp
 
-# cbind by originTime
+# Data Filter
+typhoon <- filter(typhoon_all, Year >= 1990)
 
 
 # Data Retyping
 # Typhoon
 typhoon$Year <- as.character(typhoon$Year)
+typhoon$Alarm_Start <- as.Date(typhoon$Alarm_Start)
+typhoon$Alarm_End <- as.Date(typhoon$Alarm_End)
 typhoon$Min_Pressure_NT <- as.numeric(typhoon$Min_Pressure_NT)
 typhoon$Max_Wind_Speed_NT <- as.numeric(typhoon$Max_Wind_Speed_NT)
 typhoon$Level.7_Storm_Radius_NT <- as.numeric(typhoon$Level.7_Storm_Radius_NT)
 typhoon$Level.10_Storm_Radius_NT <- as.numeric(typhoon$Level.10_Storm_Radius_NT)
 typhoon$Alarm_Counts <- as.numeric(typhoon$Alarm_Counts)
 
+
+
 # replace `NA` to `0`
 # https://bbs.pinggu.org/thread-3589221-1-1.html
 #dat[is.na(dat)] <- 0
 typhoon[is.na(typhoon)] <- 0
+
 
 
 
@@ -72,4 +79,6 @@ typhoon[is.na(typhoon)] <- 0
 # Problems
 # 1. cannot get the table data from website "https://rdc28.cwb.gov.tw/TDB/public/warning_typhoon_list/"
 # Solution: Manually copy and paste to `Numbers` then export to `csv` file
-# 2. 
+# 2. reading the nodes in `xml`
+# Solution : Read the root node of each record first, then use `xml_find_first` & `xml_find_all`
+# If take `xml_find_all` without reading the root node of each record, will get wrong value
